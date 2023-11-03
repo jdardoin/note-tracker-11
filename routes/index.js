fcb89623dc65b408ec40f86daa70c0a6a55ce9c4
jdardoin <jdardoin@icloud.com>
2023-11-03 18:55:33 -0500

    created get/post routes

--- routes/index.js
+++ routes/index.js
@@ -1,1 +1,5 @@
 const router = require('express').Router();
+const noteroutes = require('./noteroutes.js')
+router.use('/notes', noteroutes)
+
+module.exports = router
---
+++ routes/noteroutes.js
@@ -0,0 +1,24 @@
+const router = require('express').Router();
+const notesData = require('../db/db.json')
+
+const fs = require('fs')
+const {v4:uuidv4} = require('uuid')
+
+router.get('/', (req, res) => res.status(200).json(notesData))
+
+router.post('/', (req, res) => {
+const {title, text} = req.body
+const newNote = {
+    title, text, id:uuidv4()
+}
+const payload = {
+    status:'success',
+    body: newNote
+}
+notesData.push(newNote)
+fs.writeFileSync('./db/db.json', JSON.stringify(notesData))
+res.json(payload)
+})
+
+
+module.exports = router
