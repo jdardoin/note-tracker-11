8f28b2fe81331ddd51e201db51c2c2653eb5b9ab
jdardoin <jdardoin@icloud.com>
2023-11-03 18:27:13 -0500

    added routes

---
+++ server.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const path = require('path');
+
+
+const apiroute = require('./routes/index.js');
+const app = express();
+const PORT = 3001;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static('public'));
+app.use('/api', apiroute)
+
+//app.get('/api/notes', (req, res) => ('/public/js/notes.html'));
+app.get('/', (req, res) =>
+  res.sendFile(path.join(__dirname, '/public/index.html'))
+);
+app.get('/notes', (req, res) =>
+  res.sendFile(path.join(__dirname, '/public/notes.html'))
+);
+
+app.listen(PORT, () => {
+    console.log('app listen on port 3001')
+});
